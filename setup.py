ac8752bc216cfbf2495a1deabe456500d671662b
qwq212548 <qlapzpqm@outlook.com>
2025-04-26 21:47:18 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="netwatchdog",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],
+    author="Your Name",
+    description="A simple network health checker for Linux servers.",
+    url="https://github.com/yourname/NetWatchdog",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: POSIX :: Linux",
+    ],
+)
